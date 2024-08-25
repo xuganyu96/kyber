772f083200c614fda21ea057d7ea9c468825968b
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2024-08-25 17:46:57 -0400

    On arch==__arm__64 compile the CPU cycles using ARM aseemblies

--- ref/test/cpucycles.h
+++ ref/test/cpucycles.h
@@ -20,8 +20,15 @@ static inline uint64_t cpucycles(void) {
 static inline uint64_t cpucycles(void) {
   uint64_t result;
 
-  __asm__ volatile ("rdtsc; shlq $32,%%rdx; orq %%rdx,%%rax"
-    : "=a" (result) : : "%rdx");
+  
+  #if defined(__arm64__) || defined(__aarch64__)
+    __asm__ volatile ("isb; mrs %0, CNTVCT_EL0" : "=r"(result));
+  #elif defined(__x86_64__)
+    __asm__ volatile ("rdtsc; shlq $32,%%rdx; orq %%rdx,%%rax"
+      : "=a" (result) : : "%rdx");
+  #else
+    #error "Unsupported architecture"
+  #endif
 
   return result;
 }
